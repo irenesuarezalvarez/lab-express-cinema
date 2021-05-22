611e80599ef739bb961129eb06527d81fc42bb52
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-05-22 17:10:04 +0200

    Do iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const Movie = require('../models/Movie.model');//needed?
+ 
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies', (req, res, next) => {
+    Movie.find({}, (err, movies)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log('aqui', movies);
+            res.render('movies', { movies })
+        }
+        
+    })
+   // res.render('movies')
+});
+
 
 module.exports = router;
